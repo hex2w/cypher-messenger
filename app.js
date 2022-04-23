2c4269bfb3763a907549f63231d59737578f9ef9
hex3928 <n7f@protonmail.com>
2022-04-23 15:15:00 +0200

    log socket connections

--- app.js
+++ app.js
@@ -21,7 +21,18 @@ io.on("connection", socket => {
     socket.on("disconnect", _ => console.log("user disconnected") )
 })
 
+io.on("connection", (socket) => {
+    console.log(`user ${socket.id} connected`)
+    socket.on("message", (message) => {
+      console.log(message)
+      io.emit("message", `${socket.id.substring(0, 1)}: ${message}`)
+    })
+  
+    socket.on("disconnect", (_) => console.log(`user ${socket.id} disconnected`))
+})
 
-server.use(router.routes())
+server
+      .use(compose(middleware))
+      .use(router.routes())
       .use(router.allowedMethods())
       .listen(3000)
